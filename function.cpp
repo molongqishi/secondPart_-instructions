013eaf81ac09fefeb121348a57dbb42555cdc2d5
molongqishi <108988605+molongqishi@users.noreply.github.com>
2022-09-20 00:08:21 +0800

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+void greeting()
+{
+    cout << "hello" << endl;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "function.h"
+using namespace std;
+
+int main()
+{
+    greeting();
+    return 0;
+}
